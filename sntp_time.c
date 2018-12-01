a54256d0ffa5ab8c51738ea67ae5aebb6f023043
wowa <waldemar.gruenwald@ubirch.com>
2018-12-01 12:33:04 +0100

    modified to new storage functionality

--- sntp_time.c
+++ sntp_time.c
@@ -77,7 +77,7 @@ void time_status(void) {
     time(&now);
     localtime_r(&now, &timeinfo);
 
-    ESP_LOGI(TAG, "%02d.%02d.%04d %02d:%02d:%02d GMT\r\n", timeinfo.tm_mday, timeinfo.tm_mon,
-             (1900 + timeinfo.tm_year),
-             timeinfo.tm_hour, timeinfo.tm_min, timeinfo.tm_sec);
+    printf("Current time: %02d.%02d.%04d %02d:%02d:%02d GMT\r\n", timeinfo.tm_mday, timeinfo.tm_mon,
+           (1900 + timeinfo.tm_year),
+           timeinfo.tm_hour, timeinfo.tm_min, timeinfo.tm_sec);
 }
